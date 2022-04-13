b0ce22456955b7bf819a1a711cbbdc2d709960c0
david riyan kurniawan <103322177+davidriyan@users.noreply.github.com>
2022-04-13 19:34:21 +0700

    Add files via upload

---
+++ src/com/project/Main.java
@@ -0,0 +1,20 @@
+package com.project;
+public class Main{
+    public static void main(String[]args){
+        //menampilkan data orang pertama
+        String nama1 = "Budi";
+        String alamat1 = "Lampung";
+        int NIK = 123456789;
+        System.out.println( "Nama = " + nama1 );
+        System.out.println("Alamat = " + alamat1);
+        System.out.println("NIK = " + NIK);
+
+        //menampilkan data nama orang kedua
+        String nama2 = "Andi";
+        String alamat2 = "Bandung";
+        int NIK2 = 987654321;
+        System.out.println( "Nama = " + nama2 );
+        System.out.println("Alamat = " + alamat2);
+        System.out.println("NIK = " + NIK2);
+    }
+}
